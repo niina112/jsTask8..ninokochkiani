78e3ee4a66ea571e832e37d515f212abd34dc8dd
niina112 <91963048+niina112@users.noreply.github.com>
2024-12-03 15:52:54 +0400

    created new JS Task

---
+++ script.js
@@ -0,0 +1,295 @@
+const ticket = {
+  id: "e526ff35-f1a6-48a4-95f8-5d1c29581b28",
+  categoryId: "201171e2-8a78-4e15-a80e-2837c3c5abca",
+  title: "Small Granite Pizza",
+  description: "Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals",
+  location: "New Robertobury",
+  latitude: 17.6991,
+  longitude: -85.9319,
+  startsAtUtc: "2025-11-17T15:01:51.828486Z",
+  endsAtUtc: "2025-11-19T15:01:51.828505Z",
+  ticketTypes: [
+    {
+      ticketTypeId: "c6f66749-15a4-4519-bca1-adfa3e55c386",
+      name: "Basket 1",
+      price: 70,
+      currency: "GEL",
+      quantity: 150
+    },
+    {
+      ticketTypeId: "bb3093a9-6b4c-4225-8ed6-fa514b860014",
+      name: "Basket 2",
+      price: 140,
+      currency: "GEL",
+      quantity: 300
+    },
+    {
+      ticketTypeId: "795eabbb-2f4e-42e5-aa35-231f84cce0b1",
+      name: "Basket 3",
+      price: 210,
+      currency: "GEL",
+      quantity: 450
+    }
+  ],
+  images: [
+    {
+      imageId: "5df84f74-e083-4572-af04-140e8c0c3c01",
+      url: "https://picsum.photos/640/480/?image=113"
+    },
+    {
+      imageId: "7ddc3fba-6bfc-454a-a8cc-f53d63fc8b83",
+      url: "https://picsum.photos/640/480/?image=353"
+    },
+    {
+      imageId: "841ae2c1-685e-4b91-bdaa-ceeb53f9b135",
+      url: "https://picsum.photos/640/480/?image=391"
+    }
+  ],
+  lineUps: [
+    {
+      lineUpId: "7a0f89ae-9ed6-4d98-a596-84aefc6cdcf6",
+      name: "Kendrick Lamar",
+      startDate: "2025-11-17T15:01:58.142194Z",
+      endDate: "2025-11-17T17:01:58.142195Z",
+      genre: "Pop",
+      description: "Jazz"
+    },
+    {
+      lineUpId: "4d0ae57d-1c0d-4f2b-81ea-4bb6444a2f07",
+      name: "Method Man",
+      startDate: "2025-11-17T15:01:58.305345Z",
+      endDate: "2025-11-17T17:01:58.305346Z",
+      genre: "Non Music",
+      description: "World"
+    },
+    {
+      lineUpId: "f2bb20e2-1ed4-47b9-8fcf-19bcdebe14a4",
+      name: "Moku Moku",
+      startDate: "2025-11-17T15:01:58.433363Z",
+      endDate: "2025-11-17T17:01:58.433363Z",
+      genre: "Soul",
+      description: "Latin"
+    },
+    {
+      lineUpId: "b1af66fd-af5b-4448-8673-20b96cfb3f36",
+      name: "Shaggy",
+      startDate: "2025-11-17T15:01:58.558691Z",
+      endDate: "2025-11-17T17:01:58.558691Z",
+      genre: "World",
+      description: "Stage And Screen"
+    },
+    {
+      lineUpId: "f2601cae-188f-4d54-a73e-145a3bfda16d",
+      name: "Akon",
+      startDate: "2025-11-17T15:01:58.707678Z",
+      endDate: "2025-11-17T17:01:58.707679Z",
+      genre: "Pop",
+      description: "Blues"
+    },
+    {
+      lineUpId: "d9080b66-8bcd-4671-8f64-e256d6d8c149",
+      name: "Outkast",
+      startDate: "2025-11-17T15:01:58.820893Z",
+      endDate: "2025-11-17T17:01:58.820894Z",
+      genre: "Folk",
+      description: "Jazz"
+    },
+    {
+      lineUpId: "75ae5e03-ca7b-40bc-89ee-16e96a7bd6e9",
+      name: "50 Cent",
+      startDate: "2025-11-17T15:01:58.943718Z",
+      endDate: "2025-11-17T17:01:58.943719Z",
+      genre: "Reggae",
+      description: "Latin"
+    }
+  ],
+  participants: [
+    {
+      participantId: "7a0f89ae-9ed6-4d98-a596-84aefc6cdcf6",
+      name: "Kendrick Lamar",
+      role: "Singer",
+      mediaUrlCode: "artist/2YZyLoL8N0Wb9xBt1NhZWg",
+      image: {
+        imageId: "41802e06-c836-421a-b690-712cd26b7897",
+        url: "https://picsum.photos/640/480/?image=960"
+      }
+    },
+    {
+      participantId: "4d0ae57d-1c0d-4f2b-81ea-4bb6444a2f07",
+      name: "Method Man",
+      role: "Singer",
+      mediaUrlCode: "album/0Y2ArrqJQ8WuUA5tetAkZi",
+      image: {
+        imageId: "62eaa301-9aba-428f-a23f-f2adc3ef51e5",
+        url: "https://picsum.photos/640/480/?image=873"
+      }
+    },
+    {
+      participantId: "f2bb20e2-1ed4-47b9-8fcf-19bcdebe14a4",
+      name: "Moku Moku",
+      role: "Singer",
+      mediaUrlCode: "artist/2LLrSjcTIn9rTz77bX9Rsq",
+      image: {
+        imageId: "9aac0e31-dd68-4b2e-8982-0acb454d97f5",
+        url: "https://picsum.photos/640/480/?image=331"
+      }
+    },
+    {
+      participantId: "b1af66fd-af5b-4448-8673-20b96cfb3f36",
+      name: "Shaggy",
+      role: "Singer",
+      mediaUrlCode: "artist/5EvFsr3kj42KNv97ZEnqij",
+      image: {
+        imageId: "2d4a2323-3da4-442c-8881-70cce431a958",
+        url: "https://picsum.photos/640/480/?image=284"
+      }
+    },
+    {
+      participantId: "f2601cae-188f-4d54-a73e-145a3bfda16d",
+      name: "Akon",
+      role: "Singer",
+      mediaUrlCode: "artist/0z4gvV4rjIZ9wHck67ucSV",
+      image: {
+        imageId: "532d0c2b-41de-44ec-ba93-62c386b9fac6",
+        url: "https://picsum.photos/640/480/?image=1003"
+      }
+    },
+    {
+      participantId: "d9080b66-8bcd-4671-8f64-e256d6d8c149",
+      name: "Outkast",
+      role: "Singer",
+      mediaUrlCode: "artist/1G9G7WwrXka3Z1r7aIDjI7",
+      image: {
+        imageId: "df565e7f-c29f-40d6-bde3-ac54af2d4b24",
+        url: "https://picsum.photos/640/480/?image=280"
+      }
+    },
+    {
+      participantId: "75ae5e03-ca7b-40bc-89ee-16e96a7bd6e9",
+      name: "50 Cent",
+      role: "Singer",
+      mediaUrlCode: "artist/3q7HBObVc0L8jNeTe5Gofh",
+      image: {
+        imageId: "18246ae3-7e11-488e-962f-1f65828989f9",
+        url: "https://picsum.photos/640/480/?image=508"
+      }
+    }
+  ]
+}
+
+
+//Access specific properties in a nested object (like price, start date, etc.)
+
+console.log(ticket.ticketTypes[2].ticketTypeId);
+console.log(ticket.images[0].imageId);
+console.log(ticket.lineUps[4].startDate);
+console.log(ticket.participants[1].name);
+
+/// Use array methods (push, pop) to manipulate data.
+
+ticket.ticketTypes.pop()
+console.log(ticket.ticketTypes);
+
+ticket.lineUps[0].startDate = '2026-01-20';
+console.log(ticket.lineUps[0].startDate);
+
+// New ticket added at the end of the array.
+
+ticket.lineUps.push( {
+  lineUpId: "75ae5e03-ca7b-40bc-89ee-16e96a7bd6e9",
+  name: "200 Cent",
+  startDate: "2026-11-17T15:01:58.943718Z",
+  endDate: "2026-11-17T17:01:58.943719Z",
+  genre: "Drama",
+  description: "Georgian"
+}
+);
+console.log(ticket.lineUps);
+
+//Part 1: Accessing Object Properties
+//Access the price of "Basket 1" ticket: 
+
+console.log(ticket.ticketTypes[0].price)
+
+//Write a function to retrieve the price of the Basket 1 ticket type.
+const basket1 = (ticket) => {
+  console.log(ticket.ticketTypes[0].ticketTypeId);
+};
+basket1(ticket);
+
+/*
+Access the start date of the event:
+Write a function that prints the start date of the event.
+*/
+
+const start = (ticket) => {
+  console.log(ticket.lineUps[2].startDate)
+}
+start(ticket)
+
+/*
+Part 2: Using Array Methods (push, pop)
+Add a New Ticket Type:
+*/
+
+ticket.ticketTypes.push( {
+  ticketTypeId: "NEW TYPE",
+  name: "Basket 4",
+  price: 250,
+  currency: "GEL",
+  quantity: 450
+});
+console.log(ticket.ticketTypes)
+
+
+//Use the push method to add a new ticket type to the ticketTypes array. The new ticket should have a price of 300 GEL and a quantity of 500.
+
+
+ticket.ticketTypes.push( {
+  ticketTypeId: "NEW TYPE2",
+  name: "Basket 5",
+  price: 300,
+  currency: "GEL",
+  quantity: 500
+});
+console.log(ticket.ticketTypes)
+
+//Remove a Ticket Type:
+//Use the pop method to remove the last ticket type from the ticketTypes array.
+
+ticket.ticketTypes.pop()
+console.log(ticket.ticketTypes)
+
+/*
+Part 3: More Array Methods (map, filter)
+List All Ticket Names:
+Use the map method to create a new array containing only the names of the ticket types.
+*/
+const allNames = ticket.ticketTypes.map(ticketType => ticketType.name);
+console.log(allNames);
+
+//Find All Tickets Over 100 GEL:
+//Use the filter method to create a new array with all ticket types that have a price greater than 100 GEL. 
+
+
+const priceGell = ticket.ticketTypes.filter(ticketType => ticketType.price > 100);
+console.log(priceGell);
+
+/* 
+Part 4: Nested Object Access
+Access the First Participant's Name:
+Write a function to retrieve the name of the first participant in the participants array.
+*/
+
+const participantName = (participants) => {
+  console.log(ticket.participants[0].name)
+}
+participantName()
+
+//Access the Genre of the Second LineUp:
+//Write a function to access the genre of the second lineup in the lineUps array.
+
+const secondLineup = (lineUps) => {
+  console.log(ticket.lineUps[1].genre)
+}
+secondLineup()
